86c9cce475851d284f0ba72b35a570c6c259956b
FoxIS <andrius.mikonis@gmail.com>
2019-03-22 23:43:22 +0200

    ADD: Calibration scripts.

--- src/node/run_calib.sh
+++ src/node/run_calib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/pi/AutonomousTank/src/node
-python3 -m EasyVision.bin.server $HOSTNAME builder.json -H `hostname -I`
+python3 -m EasyVision.bin.server $HOSTNAME builder_calib.json -H `hostname -I`
